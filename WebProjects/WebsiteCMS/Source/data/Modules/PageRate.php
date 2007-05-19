5f532792bfaf4dbc2d990aeb7ab8db7bc133b5fd
porfirioribeiro <porfirioribeiro@fe5bbf2e-9c30-0410-a8ad-a51b5c886b2f>
2007-05-19 10:53:28 +0000

    ResDB now Fixed

--- WebProjects/WebsiteCMS/Source/data/Modules/PageRate.php
+++ WebProjects/WebsiteCMS/Source/data/Modules/PageRate.php
@@ -12,7 +12,7 @@
 
 if (isset($_GET["RatePage"]) && isset($_GET["page"]) && isset($_GET["path"])){
 	include $_GET["path"]."data/Functions/ResDB.php";
-	$db=new ResDB(dirname(__FILE__)."/"."../db/PageRater.db.php");
+	$db=new ResDB(dirname(__FILE__)."/"."../db/PageRater.db");
 	$page=$db->getMap($_GET["page"]);
 	$ips=explode(",",$page->get("ip",""));
 	foreach ($ips as $ipc){
@@ -45,7 +45,7 @@ if (isset($_GET["RatePage"]) && isset($_GET["page"]) && isset($_GET["path"])){
 			$this->page=$page;
 			$this->side=Module::LEFT;
 			$this->title="Rate this Page";	
-			$this->dbPath=dirname(__FILE__)."/"."../db/PageRater.db.php";
+			$this->dbPath=dirname(__FILE__)."/"."../db/PageRater.db";
 			$this->db=new ResDB($this->dbPath);
 			$this->pg=$this->db->getMap($this->page->id);
 			$url=str_replace($_SERVER["DOCUMENT_ROOT"], "", preg_replace("/\\\/","/",__FILE__));
