3034e8d7995619f0e83f0e607e0499cb5314205a
porfirioribeiro <porfirioribeiro@fe5bbf2e-9c30-0410-a8ad-a51b5c886b2f>
2007-05-24 19:25:01 +0000

    mnu

--- WebProjects/WebsiteCMS/Source/admin/Modules/menuEditor.php
+++ WebProjects/WebsiteCMS/Source/admin/Modules/menuEditor.php
@@ -14,17 +14,19 @@ class menuEditor extends Module {
 	function finish(){
 		$this->db->close();
 	}
-	function expandMenu($mnu){
+	function expandMenu($mnu,$path){
 		for ($i=1;$i<=count($mnu);$i++){
 			$val=$mnu[$i];
 			if (ArrayMap::is($val)){
 				if ($val->isMap("1")){
 					$id=preg_replace(array("/-/","/ /"),"_",$val->get("name"));
 					?>
-					<a style="display:block; text-decoration:none;" href="javascript:;" onclick="$('<?=$id?>').toggle();"><?=$val->get("name")?></a>
+					+<?=$val->get("name")?>
+					
 					<div id="<?=$id?>" style="display:;padding-left:10px">
 					<?php
-						$this->expandMenu($val);
+					
+						$this->expandMenu($val,$path+"."+$i);
 					?>
 					</div>
 					<?php				
@@ -32,7 +34,8 @@ class menuEditor extends Module {
 					$name=$val->get("name");
 					$url =$val->get("url"); 			
 					?>
-					<a style="display:block;" href="<?=$val->get("url")?>"><?=$val->get("name")?></a>
+					- <?=$i.$name?>-<?=$url?>"
+					<br>
 					<?php	
 				}
 			}
@@ -40,7 +43,7 @@ class menuEditor extends Module {
 	}
 	function content(){
 	?>
-		<?=$this->expandMenu($this->db)?>
+		<?=$this->expandMenu($this->db,"")?>
 	<?php
 	}
 }
