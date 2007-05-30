494e65258fee67a8896e23211554b4aa66ecce5c
porfirioribeiro <porfirioribeiro@fe5bbf2e-9c30-0410-a8ad-a51b5c886b2f>
2007-05-30 19:56:07 +0000

    Lots of work on db editor

---
+++ WebProjects/WebsiteCMS/Source/admin/tpl/db.tpl
@@ -0,0 +1,12 @@
+#{start:dbList_addDB}
+<div>/#{category} <img onclick="$('DB_CATEGORY_#{category}').toggle();" alt="Add Database" title="Add Database" border="0" style="vertical-align:middle;cursor:pointer;" src="icons/add.png"></div>
+<form action="index.php" id="DB_CATEGORY_#{category}" style="display: none;margin: 0px 0px 0px 10px;">
+	<input type="hidden" name="manage" value="db">
+	<input type="hidden" name="action" value="addDB">
+	<input type="hidden" name="category" value="#{category}">
+	<img alt="Cancel" title="Cancel" src="icons/button_cancel.png" style="cursor: pointer;" onclick="$('DB_CATEGORY_#{category}').hide();return false;">
+	<input type="text" name="db" size="10" title="The name of the database" style="height: 13px;">
+	<input type="checkbox" name="tabled" title="Check this if you want that the created Database becames a Tabled Database">
+	<img alt="Submit" title="Create the database" src="icons/button_ok.png" style="cursor: pointer;" onclick="$('DB_CATEGORY_#{category}').submit();">
+</form>	
+#{end:dbList_addDB}
--- WebProjects/WebsiteCMS/Source/data/Functions/GetFolders.php
+++ WebProjects/WebsiteCMS/Source/data/Functions/GetFolders.php
@@ -4,7 +4,7 @@
 * This script will return an array of files or folders in a directory you send it
 * Licenced under GPLv2 read GPL.txt for details
 * @version 1
-* @copyright © 2007 ResPlace Team
+* @copyright ï¿½ 2007 ResPlace Team
 * @lastedit 12-05-07
 */
 
@@ -29,11 +29,11 @@ if(is_array($contents))	{
 */
 	
 function GetFolders($dir='.'){
-	$files="";	
+	$files=array();	
 	if(is_dir($dir)){
 		$dirlist = opendir($dir);
 		while( ($file = readdir($dirlist)) !== false){
-			if(is_dir($dir.$file) && strpos($file,".svn")===false){
+			if(is_dir($dir.$file) && $file!=".." && $file!="." && strpos($file,".svn")===false){
 				$files[] = $file;
 			}				
 		}			
@@ -45,16 +45,18 @@ function GetFolders($dir='.'){
 
 //GET FILES
 
-function GetFiles($dir='.'){
-    $files="";	
+function GetFiles($dir='.',$mask="*"){
+
+    $mask=preg_replace("/\*/",".*",$mask);
+	$files=array();	
     if(is_dir($dir)){
     	$dirlist = opendir($dir);
 		while( ($file = readdir($dirlist)) !== false){
-			if(!is_dir($dir.$file)){
+			if(!is_dir($dir.$file) && preg_match("/^".$mask."$/",$file)){
 				$files[] = $file;
 			}
 		}
-		($sortorder == 0) ? asort($files) : rsort($files);
+		asort($files);
 		return $files;
 	} else {
 		return FALSE;
