55661d6c9cf1e7784be2363fe729512a81d49c17
porfirioribeiro <porfirioribeiro@fe5bbf2e-9c30-0410-a8ad-a51b5c886b2f>
2007-06-02 20:19:56 +0000

    Removed file/folder

--- WebProjects/WebsiteCMS/Source_/OpenWebMS/Test_config.php
+++
@@ -1,8 +0,0 @@
-<?php
-include dirname(__FILE__).'/config.php';
-foreach ($WebMS as $key=>$value) {
-	if (strpos($key,"Path")!==false){
-    echo "Test ".$key." with the path ".$value.": ".((is_dir($value))?"Suceed!":"Fail!")."<br>";
-  }
-}
-?>
