6a044f4cd584ce380985a54ff1e61abc9d5840a8
porfirioribeiro <porfirioribeiro@fe5bbf2e-9c30-0410-a8ad-a51b5c886b2f>
2007-06-16 10:20:20 +0000

    Adding LayoutManager.php

---
+++ WebProjects/WebsiteCMS/Source/OpenWebMS/Admin/Modules/LayoutManager.php
@@ -0,0 +1,14 @@
+<?php
+class LayoutManager extends Module {
+	var $db;
+    function LayoutManager($page){
+    	parent::Module($page);
+    	$this->title="Manage layout";
+    	$this->db=new ResDB("Layouts","core");
+    	foreach ($this->db as $map => $key) {
+    		echo $map;
+    	}
+    	
+    }
+}
+?>
