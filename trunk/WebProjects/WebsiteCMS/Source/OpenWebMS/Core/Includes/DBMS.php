c74a69474a16009e7e6bed59b57ff318bcc3c634
deano.at.resplace <deano.at.resplace@fe5bbf2e-9c30-0410-a8ad-a51b5c886b2f>
2007-07-19 16:30:19 +0000

    apage.php : unknown modification.
    Config.php : added some MySQL vars
    
    DBMS.php : Database Management script, whoo

---
+++ trunk/WebProjects/WebsiteCMS/Source/OpenWebMS/Core/Includes/DBMS.php
@@ -0,0 +1,53 @@
+<?php
+/**
+* SQL Model for MySQL - Postresql...
+* A model for translation betweeen various database management systems (DBMS)
+* Licenced under GPLv2 read GPL.txt for details
+* @version 1
+* @copyright ? 2007 ResPlace Team
+* @lastedit 24-06-07
+*/
+
+class SQL{
+	var $model;
+	var $query="";
+	
+	function connect() {
+		global $WebMS, $db;
+		
+		if ( isset($WebMS["MySQL_Host"]) && isset($WebMS["MySQL_UserName"]) && isset($WebMS["MySQL_Host"]) && isset($WebMS["MySQL_Password"])  isset($WebMS["MySQL_Database"]) ) {
+			$dbconnect=mysql_connect($WebMS["MySQL_Host"],$WebMS["MySQL_UserName"],$WebMS["MySQL_Password"]);
+			$db=mysql_select_db($WebMS["MySQL_Database"],$dbconnect);
+			return $db;
+		} else {
+			//error
+		}
+	}
+	
+	function disconnect() {
+		global $WebMS, $db;
+		
+		mysql_close($db);
+	}
+	
+	function sql($query) {
+		$query = mysql_query($query);
+	
+		if(empty($GLOBALS['queries'])) $GLOBALS['queries'] = 0;
+		$GLOBALS['queries']++;
+		
+		//Fetch the query if needed
+		if($fetch) while($row = mysql_fetch_assoc($query)) {
+			if(empty($key)) $data[] = $row; 
+			
+			else $data[$row[$key]] = $row; 
+		}
+		
+		else $data = $query;
+	
+		//Return the result
+		return $data;
+	}
+}
+
+?>
