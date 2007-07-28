4941f586fb2be2d2ee276d4731fccf8768e0e5de
porfirioribeiro <porfirioribeiro@fe5bbf2e-9c30-0410-a8ad-a51b5c886b2f>
2007-07-28 18:18:23 +0000

    Template work

--- trunk/WebProjects/WebsiteCMS/Source/tests/templateTest.php
+++ trunk/WebProjects/WebsiteCMS/Source/tests/templateTest.php
@@ -1,11 +1,21 @@
 <?php
 
 include_once "Template.php";
+$fuck="aha i know";
+$tpl=new Template('
+	#{IIF:go,good,bad}
+	<br>
+	#{if:go}
+		#{if:go}
+			Aha
+		#{else}
+			Nah
+		#{endif}
+	#{else}
+		Nah
+	#{endif}
+');
 
-$tpl=new Template("
-	asd ${10} be
-");
-
-echo $tpl->parse(array());
+echo $tpl->parse(array("go"=>true));
 
 ?>
