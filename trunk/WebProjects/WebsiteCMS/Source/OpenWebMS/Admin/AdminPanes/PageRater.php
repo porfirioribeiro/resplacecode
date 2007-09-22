85c4a6aff13d72a76ea9e0008d899112df509541
porfirioribeiro <porfirioribeiro@fe5bbf2e-9c30-0410-a8ad-a51b5c886b2f>
2007-09-22 09:03:42 +0000

    added styles folder

--- trunk/WebProjects/WebsiteCMS/Source/OpenWebMS/Admin/AdminPanes/PageRater.php
+++ trunk/WebProjects/WebsiteCMS/Source/OpenWebMS/Admin/AdminPanes/PageRater.php
@@ -33,13 +33,44 @@ class PageRater extends Module {
 	}
 	function content() {
 		global $WebMS, $path, $adminpassword, $pageratebara, $pageratebarb;
-
 		?>
 		Below you can change various options for the PageRate Module. Please make sure you have the latest version of this module BEFORE making any module changes.<br><br>
 
-		<div class="fieldset">
+		<div style="height:200px;">
+		<div class="fieldset" style="float: left; margin-left: 10px; margin-top: 10px;">
+			<div class="ftitle"><b>Bar background style:</b></div><br>
+			<div style="overflow:scroll; width:200px; height:150px; border:2px inset #ccc">
+				<div style="padding:10px">
+					<?php
+					//NEW CODE
+					//Create list of styles for user to choose
+					$BarStyleFolders=GetFolders($WebMS["ModulesPath"] ."PageRate/Bars/");
+
+					if (count($BarStyleFolders)) {
+						foreach ($BarStyleFolders as $folder) {
+							echo "<div style='text-decoration:underline;'><b>$folder</b></div>";
+							$BarStyleFiles = GetFiles($WebMS["ModulesPath"]."PageRate/Bars/$folder/");
+							echo "<div style='padding-left:10px'>";
+
+							foreach ($BarStyleFiles as $file) {
+								$filen=explode(".",$file);
+								if (strcmp($folder.'/'.$file,$pageratebara[0].'/'.$pageratebara[1])==0) {
+									echo "<div style='border:1px solid black'><img src='".$WebMS['ModulesUrl']."PageRate/Bars/$folder/$file' /><a href='?style3=$folder&amp;style4=$file&amp;pane=PageRater' style='position:relative; top: -0.50em; left: 0.5em;'>$filen[0]</a></div>";
+								} else {
+									echo "<img src='".$WebMS['ModulesUrl']."PageRate/Bars/$folder/$file' /><a href='?style3=$folder&amp;style4=$file&amp;pane=PageRater' style='position:relative; top: -0.50em; left: 0.5em;'>$filen[0]</a><br>";
+								}
+							}
+
+							echo "</div><br>";
+						}
+					}
+					?>
+				</div>
+			</div>
+		</div>
+		<div class="fieldset" style="float: left; margin-left: 10px; margin-top: 10px;">
 			<div class="ftitle"><b>Bar foreground style:</b></div><br>
-			<div style="overflow:scroll; width:200px; height:120px; border:2px inset #ccc">
+			<div style="overflow:scroll; width:200px; height:150px; border:2px inset #ccc">
 				<div style="padding:10px">
 				<?php
 				//NEW CODE
@@ -68,38 +99,19 @@ class PageRater extends Module {
 				</div>
 			</div>
 		</div>
-		<br><br>
-
-		<div class="fieldset">
-			<div class="ftitle"><b>Bar background style:</b></div><br>
-			<div style="overflow:scroll; width:200px; height:120px; border:2px inset #ccc">
+		
+		<div class="fieldset" style="float: left; margin-left: 10px; margin-top: 10px;">
+			<div class="ftitle"><b>Preview:</b></div><br>
+			<div style="width:200px; height:154px;">
 				<div style="padding:10px">
-					<?php
-					//NEW CODE
-					//Create list of styles for user to choose
-					$BarStyleFolders=GetFolders($WebMS["ModulesPath"] ."PageRate/Bars/");
-
-					if (count($BarStyleFolders)) {
-						foreach ($BarStyleFolders as $folder) {
-							echo "<div style='text-decoration:underline;'><b>$folder</b></div>";
-							$BarStyleFiles = GetFiles($WebMS["ModulesPath"]."PageRate/Bars/$folder/");
-							echo "<div style='padding-left:10px'>";
-
-							foreach ($BarStyleFiles as $file) {
-								$filen=explode(".",$file);
-								if (strcmp($folder.'/'.$file,$pageratebara[0].'/'.$pageratebara[1])==0) {
-									echo "<div style='border:1px solid black'><img src='".$WebMS['ModulesUrl']."PageRate/Bars/$folder/$file' /><a href='?style3=$folder&amp;style4=$file&amp;pane=PageRater' style='position:relative; top: -0.50em; left: 0.5em;'>$filen[0]</a></div>";
-								} else {
-									echo "<img src='".$WebMS['ModulesUrl']."PageRate/Bars/$folder/$file' /><a href='?style3=$folder&amp;style4=$file&amp;pane=PageRater' style='position:relative; top: -0.50em; left: 0.5em;'>$filen[0]</a><br>";
-								}
-							}
-
-							echo "</div><br>";
-						}
-					}
-					?>
+					<img src="<?=$WebMS['ModulesUrl']."PageRate/Bars/".$pageratebarb[0].'/'.$pageratebarb[1]?>">
+					<img src="<?=$WebMS['ModulesUrl']."PageRate/Bars/".$pageratebarb[0].'/'.$pageratebarb[1]?>">
+					<img src="<?=$WebMS['ModulesUrl']."PageRate/Bars/".$pageratebarb[0].'/'.$pageratebarb[1]?>">
+					<img src="<?=$WebMS['ModulesUrl']."PageRate/Bars/".$pageratebara[0].'/'.$pageratebara[1]?>">
+					<img src="<?=$WebMS['ModulesUrl']."PageRate/Bars/".$pageratebara[0].'/'.$pageratebara[1]?>">
 				</div>
 			</div>
+		</div>
 		</div>
 		<?php
 	}
