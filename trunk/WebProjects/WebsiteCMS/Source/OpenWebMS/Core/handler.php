b0ec1cb9e8e5d9c5d6309cac182f61c6611ce8c9
porfirioribeiro <porfirioribeiro@fe5bbf2e-9c30-0410-a8ad-a51b5c886b2f>
2007-11-22 21:10:05 +0000

    Fixed bug

---
+++ trunk/WebProjects/WebsiteCMS/Source/OpenWebMS/Core/handler.php
@@ -0,0 +1,21 @@
+<?php
+$file = $_SERVER["PATH_TRANSLATED"];
+$exts = split("[/\\.]", strtolower($file)) ;
+$ext = $exts[count($exts)-1];
+
+if ($ext=="js" || $ext=="css"){
+  //ob_start ("ob_gzhandler");
+}
+
+
+$ext = substr($file,strrpos($file, ".")+1,strlen($file));
+ob_start ("ob_gzhandler");
+if($ext=="js") $ext="javascript";
+header("Content-type: text/".$ext."; charset: UTF-8");
+header("Content-Encoding: gzip,deflate");
+header("Expires: ".gmdate("D, d M Y H:i:s", time() + (24 * 60 * 60 * 60)) . " GMT");//adiciona 1 dia ao tempo de expiração
+header( "ETag: ".dechex( $file_last_modified ) );
+header( "Cache-Control: must-revalidate, proxy-revalidate, max-age=" . $max_age . ", s-maxage=" . $max_age );
+
+readfile($file);
+?>
