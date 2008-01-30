d575c432933421ea811486f8e576614f9f28ddf7
porfirioribeiro <porfirioribeiro@fe5bbf2e-9c30-0410-a8ad-a51b5c886b2f>
2008-01-30 16:44:36 +0000

    Initial import

---
+++ trunk/WebProjects/AutoSys/AutoSys/Admin/WebMSoptions.php
@@ -0,0 +1,24 @@
+<?php
+//allows you to grab a copy of the WebMSoptions.db file (bypass the .htaccess restriction).
+//include_once "../Core/Includes/ResDB.php";
+$path="../";
+include_once $path.'WebMS.php';
+$page=new WebMS($path,"Admin Panel");
+$db=new ResDB("WebMSoptions");
+$psswd=$db->get("adminpassword");
+//echo '1:'.$psswd.'<br>2:'.$_REQUEST['sesid'].'<br>';
+
+if ($_REQUEST['sesid']==$psswd){
+	$file="../db/WebMSoptions.db";
+	if (file_exists($file)) {
+		$fh=fopen($file,'r');
+		$filedata=fread($fh,filesize($file));
+		fclose($fh);
+		echo $filedata;
+	} else {
+		echo 'No database exists to backup (error?)';
+	}
+} else {
+	echo 'You need to be logged in as an admin ;)';
+}
+?>
