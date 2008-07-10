1c8a4efc78527e1e27f09d9bb152a48bc5546639
porfirioribeiro <porfirioribeiro@fe5bbf2e-9c30-0410-a8ad-a51b5c886b2f>
2008-07-10 15:27:38 +0000

    fized std

---
+++ trunk/WebProjects/stdPHP/test.php
@@ -0,0 +1,38 @@
+<?php
+
+    include 'std/String.php';
+    
+    has
+    $start = microtime(true); 
+
+    for ($i = 0; $i <= 1000; $i++) { 
+
+        strtoupper("My Test String | Porfirio's Klass"); 
+
+    } 
+
+    $end1 = microtime(true); 
+
+ 
+
+    for ($i = 0; $i <= 1000; $i++) { 
+
+        st("My Test String | Porfirio's Klass")->upperCase; 
+
+    } 
+
+    $end2 = microtime(true); 
+
+ 
+
+    $difference1 = $end1 - $start; 
+
+    $difference2 = $end2 - $end1; 
+
+ 
+    print "PHP: $difference1 seconds.\nPorfirio: $difference2 seconds.";
+
+    
+    $GLOBALS
+
+?>
